291fc3a1b0288d80d4cb263fbb23799f460638fb
DEGUZMAN <mykeedeguzman017@gmail.com>
2024-03-22 21:29:04 +0900

    ACTIVITY-1-DEGUZMAN

---
+++ app/assignment-page/assignment.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'assignment.page.html',
+  styleUrls: ['assignment.page.scss'],
+
+})
+export class assignmentPage {
+  groupName = "CODEA8E";
+  constructor() {}
+}
