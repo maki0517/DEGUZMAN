8634871b23ee039256e04fe5a25d8f6696547199
maki0517 <mykeedeguzman017@gmail.com>
2024-04-30 11:38:57 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ACTIVITY8DEGUZMAN',
+  webDir: 'www'
+};
+
+export default config;
