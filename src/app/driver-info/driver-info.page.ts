5d4902d351cb7ea11c1e7e944df335d5e5df901c
KaruKasu <mykeedeguzman017@gmail.com>
2024-05-18 15:20:48 +0800

    driver info page

--- src/app/driver-info/driver-info.page.ts
+++ src/app/driver-info/driver-info.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-driver-info',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./driver-info.page.scss'],
 })
 export class DriverInfoPage implements OnInit {
+  drivers: any[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.fetchDrivers();
   }
 
+  async fetchDrivers() {
+    const app = initializeApp(environment.firebaseConfig);
+    const db = getFirestore(app);
+
+    try {
+      const q = query(collection(db, 'users'), where('userType', '==', 'driver'));
+      const querySnapshot = await getDocs(q);
+      const fetchedDrivers = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        carType: doc.data()['carType'],
+        isAvailable: doc.data()['isAvailable'],
+        phone: doc.data()['phone'],
+        userEmail: doc.data()['userEmail'],
+        username: doc.data()['username'],
+      }));
+      this.drivers = fetchedDrivers;
+    } catch (error) {
+      console.error('Error fetching drivers:', error);
+    }
+  }
 }
