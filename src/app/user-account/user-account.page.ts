62bdea7f2f751c34e228d3191de16674887d1a24
KaruKasu <mykeedeguzman017@gmail.com>
2024-05-18 16:09:31 +0800

    back buttons

--- src/app/user-account/user-account.page.ts
+++ src/app/user-account/user-account.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, query, where, getDocs, updateDoc } from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-user-account',
@@ -6,10 +9,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-account.page.scss'],
 })
 export class UserAccountPage implements OnInit {
+  user: any = null;
+  isEditing: boolean = false;
+  userEmail: string = '';
+  username: string = '';
+  phone: string = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.fetchUserDetails();
   }
 
+  async fetchUserDetails() {
+    const app = initializeApp(environment.firebaseConfig);
+    const db = getFirestore(app);
+    const emailFromLocalStorage = localStorage.getItem('email');
+
+    if (emailFromLocalStorage) {
+      const q = query(collection(db, 'users'), where('userEmail', '==', emailFromLocalStorage));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        const userData = querySnapshot.docs[0].data();
+        this.user = userData;
+        this.userEmail = userData['userEmail'];
+        this.username = userData['username'];
+        this.phone = userData['phone'];
+      } else {
+        console.error('No user found in Firestore for the provided email');
+      }
+    } else {
+      console.error('No email found in local storage');
+    }
+  }
+
+  enableEditing() {
+    this.isEditing = true;
+  }
+
+  async saveUserDetails() {
+    const app = initializeApp(environment.firebaseConfig);
+    const db = getFirestore(app);
+    const emailFromLocalStorage = localStorage.getItem('email');
+  
+    if (emailFromLocalStorage) {
+      const q = query(collection(db, 'users'), where('userEmail', '==', emailFromLocalStorage));
+      const querySnapshot = await getDocs(q);
+  
+      if (!querySnapshot.empty) {
+        const userDoc = querySnapshot.docs[0].ref;
+        await updateDoc(userDoc, {
+          userEmail: this.userEmail,
+          username: this.username,
+          phone: this.phone,
+        });
+        this.isEditing = false;
+        this.fetchUserDetails();
+      } else {
+        console.error('No user found in Firestore for the provided email');
+      }
+    } else {
+      console.error('No email found in local storage');
+    }
+  }
 }
